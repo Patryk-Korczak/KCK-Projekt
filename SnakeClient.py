d0256d12072174f7f9b4e79aff2ecdaacf17b491
Patryk Korczak <46904287+Patryk-Korczak@users.noreply.github.com>
2020-07-11 00:46:05 +0200

    Add files via upload
    Added sounds, multiple messages. Added after death loop. Fixed minor bugs.

--- SnakeClient.py
+++ SnakeClient.py
@@ -6,9 +6,10 @@ import pygame
 import socket
 import pickle
 import time
+import pygame.freetype
 
 
-# This is host file for Multi Snake game. Run this file first. There's 5 seconds timeout allowing to start
+# This is client file for Multi Snake game. Run this file second. There's 5 seconds timeout allowing to start
 # SnakeClient in time.
 
 class Snake(object):
@@ -21,8 +22,8 @@ class Snake(object):
         self.snake_body = []  # List containing snake position
         self.length = 1  # Snake length
         self.alive = True  # is player still in the game
-        self.apple_x = 510
-        self.apple_y = 380
+        self.apple_x = 510  # apple position x
+        self.apple_y = 380  # apple position y
 
 
 class AsyncHost(Thread):
@@ -53,29 +54,46 @@ class AsyncHost(Thread):
 
 
 my_snake = Snake()
-my_snake.color = (0, 255, 0)  # green
+my_snake.color = (0, 200, 0)  # green
 my_snake.x = 760
 my_snake.y = 380
 
 display_width = 1020
 display_height = 760
 server = AsyncHost()
+server.setDaemon(True)
 server.start()
 time.sleep(5)
 
 enemy_snake = server.get_data()
 pygame.init()
+pygame.mixer.init()
+apple_effect = pygame.mixer.Sound('sounds/apple.wav')
+apple_effect.set_volume(0.05)
+lost_effect = pygame.mixer.Sound('sounds/lost.wav')
+lost_effect.set_volume(0.05)
 display = pygame.display.set_mode((display_width, display_height))
 pygame.display.update()
 pygame.display.set_caption('MultiSnake - Client')
 game_over = False
+update_on_death = True  # flag to make sure player values on death only get updated once
 block_length = 10  # 10 pixels for 1 block of snake
 clock = pygame.time.Clock()
-game_speed = 12
+game_speed = 30
+background = pygame.image.load('resources/game_background.png')
+apple_image = pygame.image.load('resources/apple.png')
+FONT = pygame.freetype.SysFont('Microsoft Sans Serif', 32)
+
 
 def new_apple():
     my_snake.apple_x = round(random.randrange(0, display_width - block_length) / 10.0) * 10.0
-    my_snake.apple_y = round(random.randrange(0, display_width - block_length) / 10.0) * 10.0
+    my_snake.apple_y = round(random.randrange(0, display_height - block_length) / 10.0) * 10.0
+
+
+def print_score():
+    text = 'MultiSnake Current Score: ' + str(my_snake.length + -1) + ' Enemy Score: ' + str(enemy_snake.length - 1)
+    pygame.display.set_caption(text)
+
 
 while not game_over:
     enemy_snake = server.get_data()  # update enemy snake position
@@ -101,6 +119,7 @@ while not game_over:
     if my_snake.x > display_width or my_snake.x < 0 or my_snake.y > display_height or my_snake.y < 0:
         my_snake.alive = False  # wrap this into function
 
+    print_score()
 
     my_snake.x += my_snake.x_change
     my_snake.y += my_snake.y_change
@@ -116,27 +135,63 @@ while not game_over:
         if x == my_snake_top:
             my_snake.alive = False
 
-
-
-    display.fill((255, 255, 255))  # clear display with whit color
+    for x in enemy_snake.snake_body[:]:
+        if x == my_snake_top:
+            my_snake.alive = False
+    display.fill((255, 255, 255))  # clear display with white color
+    display.blit(background, (0, 0))
+    display.blit(apple_image, (floor(enemy_snake.apple_x), floor(enemy_snake.apple_y)))
     if my_snake.alive:
         for x in my_snake.snake_body:
             pygame.draw.rect(display, my_snake.color, [x[0], x[1], block_length, block_length])
     if enemy_snake.alive:
         for x in enemy_snake.snake_body:
             pygame.draw.rect(display, enemy_snake.color, [x[0], x[1], block_length, block_length])
-    pygame.draw.rect(display, (255, 0, 0), [floor(enemy_snake.apple_x), floor(enemy_snake.apple_y), block_length, block_length])
+
     if not my_snake.alive and not enemy_snake.alive:
         game_over = True
+        pygame.display.update()
+        display.fill((255, 255, 255))  # clear display with white color
+        display.blit(background, (0, 0))
+        FONT.render_to(display, (10, 50),
+                       'Game over! Your score: ' + str(my_snake.length - 1) + ' Enemy score: ' + str(
+                           enemy_snake.length - 1),
+                       (255, 0, 0))
+        FONT.render_to(display, (10, 100), 'Press SPACE to exit...', (255, 0, 0))
 
     if my_snake.x == my_snake.apple_x and my_snake.y == my_snake.apple_y:
         my_snake.length += 1
         print("Your snake ate apple...")
+        apple_effect.play()
         new_apple()
 
     if enemy_snake.x == my_snake.apple_x and enemy_snake.y == my_snake.apple_y:
         print("Enemy snake ate apple...")
         new_apple()
 
+    if not my_snake.alive:
+        my_snake.snake_body.clear()
+        my_snake.x = 1
+        my_snake.y = 1
+        if enemy_snake.alive:
+            FONT.render_to(display, (10, 10), 'You lost! Your score: ' + str(my_snake.length - 1), (255, 0, 0))
+        if update_on_death:
+            lost_effect.play()
+            update_on_death = False
+
     pygame.display.update()
     clock.tick(game_speed)
+
+while game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                pygame.quit()
+                quit()
+
+    display.fill((255, 255, 255))  # clear display with whit color
+    display.blit(background, (0, 0))
+    FONT.render_to(display, (10, 10), 'Game over! Your score: ' + str(my_snake.length - 1) + 'Enemy score: ' + str(enemy_snake.length -1), (255, 0, 0))
+    FONT.render_to(display, (10, 10), 'Press SPACE to exit...', (255, 0, 0))
+
+
